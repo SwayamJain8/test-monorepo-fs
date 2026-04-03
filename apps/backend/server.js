aafdcc9eb5d93018ee85b4946a3e0dd6f2f5ea60
Swayam <160944160+SwayamJain8@users.noreply.github.com>
2026-04-04 02:44:42 +0530

    Update server to use PORT from environment variables
    Use environment variable for server port configuration.

--- apps/backend/server.js
+++ apps/backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv"
 
+dotenv.config()
 const app = express();
-const PORT = 5000;
+
+const PORT = process.env.PORT;
 
 // In-memory storage (resets when the server restarts)
 let todos = [];
